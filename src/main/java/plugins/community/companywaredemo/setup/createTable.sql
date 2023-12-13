283e67a72fd322ce0d91b6e7da5fd3e0ace68a2f
mbauer <mathias.bauer@mbdus.de>
2023-12-13 18:07:15 +0100

    removed files

--- src/main/java/plugins/community/companywaredemo/setup/createTable.sql
+++ src/main/java/plugins/community/companywaredemo/setup/createTable.sql
@@ -8,10 +8,3 @@ CREATE TABLE Demo (id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY(Start with 1,
 				   city VARCHAR(255),
 				   country VARCHAR(255)
 );
-
-INSERT INTO Demo (salutation, name, firstname, street, streetnumber, zipcode, city, country)    
-  (SELECT 'Herr' as salutation, 'Mustermann' as name, 'Hans' as firstname, 'Musterstraße' as street, '1' as streetnumber, '11111' as zipcode, 'Musterhausen' as city, 'Musterland' as country
-   FROM Demo
-   WHERE name = 'Mustermann'
-   HAVING count(*)=0  
-);
