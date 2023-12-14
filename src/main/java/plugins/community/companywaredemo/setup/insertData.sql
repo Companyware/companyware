4615bb5eb605abb3a4bd67b25cf262112ba386f6
mbauer <mathias.bauer@mbdus.de>
2023-12-14 07:29:01 +0100

    changing directory for plugins

--- src/main/java/plugins/community/companywaredemo/setup/insertData.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO Demo (salutation, name, firstname, street, streetnumber, zipcode, city, country)    
-  (SELECT 'Herr' as salutation, 'Mustermann' as name, 'Hans' as firstname, 'Musterstraße' as street, '1' as streetnumber, '11111' as zipcode, 'Musterhausen' as city, 'Musterland' as country
-   FROM Demo
-   WHERE name = 'Mustermann'
-   HAVING count(*)=0  
-);
